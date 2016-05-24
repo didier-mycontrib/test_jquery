3f6635aafca4f9e8c59ed857c257324982665dad
didier <didier-defrance@wanadoo.fr>
2016-05-24 15:11:42 +0200

    with sync_github script

---
+++ sync_push_toGitHub.bat
@@ -0,0 +1,4 @@
+git remote add GitHubMyContribOrigin https://github.com/didier-mycontrib/test_jquery.git
+git push -u GitHubMyContribOrigin master
+REM didier-mycontrib / gh14.....sm..x / didier@d-defrance.fr
+pause
